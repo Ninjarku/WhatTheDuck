b3d40b0fe47c8476f6b378c6494d73d8bf50db07
Gavin <116007416+gvintng@users.noreply.github.com>
2024-06-25 16:12:18 +0800

    Added duckdb.sql

---
+++ duckdb.sql
@@ -0,0 +1,157 @@
+CREATE DATABASE  IF NOT EXISTS `ict2216db` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ict2216db`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ict2216db
+-- ------------------------------------------------------
+-- Server version	8.0.37-0ubuntu0.23.10.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Cart`
+--
+
+DROP TABLE IF EXISTS `Cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Cart` (
+  `Cart_ID` int NOT NULL AUTO_INCREMENT,
+  `User_ID` int unsigned NOT NULL,
+  `Product_ID` int NOT NULL,
+  `Quantity` int DEFAULT NULL,
+  `Price` float DEFAULT NULL,
+  `Total_Price` float DEFAULT NULL,
+  PRIMARY KEY (`Cart_ID`),
+  UNIQUE KEY `Cart_ID_UNIQUE` (`Cart_ID`),
+  UNIQUE KEY `Product_ID_UNIQUE` (`Product_ID`),
+  UNIQUE KEY `User_ID_UNIQUE` (`User_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cart`
+--
+
+LOCK TABLES `Cart` WRITE;
+/*!40000 ALTER TABLE `Cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Order`
+--
+
+DROP TABLE IF EXISTS `Order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Order` (
+  `Order_ID` int NOT NULL AUTO_INCREMENT,
+  `User_ID` int NOT NULL,
+  `Product_ID` int NOT NULL,
+  `Quantity` int DEFAULT NULL,
+  `Total_Price` float DEFAULT NULL,
+  `Payment_Type` varchar(45) DEFAULT NULL,
+  `Billing_Address` varchar(255) DEFAULT NULL,
+  `Order_Status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Order_ID`),
+  UNIQUE KEY `Order_ID_UNIQUE` (`Order_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Order`
+--
+
+LOCK TABLES `Order` WRITE;
+/*!40000 ALTER TABLE `Order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Product`
+--
+
+DROP TABLE IF EXISTS `Product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Product` (
+  `Product_ID` int NOT NULL AUTO_INCREMENT,
+  `Product_Name` varchar(90) NOT NULL,
+  `Product_Description` varchar(8000) NOT NULL,
+  `Product_Image` blob,
+  `Price` float unsigned DEFAULT NULL,
+  `Quantity` int unsigned DEFAULT NULL,
+  `Product_Category` varchar(45) DEFAULT NULL,
+  `Product_Available` tinyint DEFAULT NULL,
+  PRIMARY KEY (`Product_ID`),
+  UNIQUE KEY `Product_ID_UNIQUE` (`Product_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Product`
+--
+
+LOCK TABLES `Product` WRITE;
+/*!40000 ALTER TABLE `Product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `User` (
+  `User_ID` int unsigned NOT NULL AUTO_INCREMENT,
+  `Username` varchar(45) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Mobile_Number` int unsigned NOT NULL,
+  `Billing_Address` varchar(255) DEFAULT NULL,
+  `Gender` varchar(45) DEFAULT NULL,
+  `Profile_Image` blob,
+  `Enable_OTP` tinyint DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  `User_Type` varchar(45) DEFAULT NULL,
+  `Account_Active` tinyint DEFAULT NULL,
+  PRIMARY KEY (`User_ID`),
+  UNIQUE KEY `User_ID_UNIQUE` (`User_ID`),
+  UNIQUE KEY `Username_UNIQUE` (`Username`),
+  UNIQUE KEY `Email_UNIQUE` (`Email`),
+  UNIQUE KEY `Mobile_Number_UNIQUE` (`Mobile_Number`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (2,'test2','$2y$10$GgGwauJmgvkSuCQexuUMkOAGnEcIfqUU6GJSw48y5/FYVVy2hw3Jy','test2@test.com',12345677,NULL,NULL,NULL,0,'2024-05-08','Customer',NULL),(3,'test3','$2y$10$tVGYWg6FMvdqkiCkrmHPqevUGqYi9A/AMpxSNVUFwMls8Mpo0nYYC','test3@test.com',12345655,NULL,NULL,NULL,0,'2024-04-10','Customer',NULL),(4,'admin','$2y$10$o60GEVftshyFXFlwdAoqLuj/4wMQwuvAsqd3CaMFxqOz8U2r8xMbm','admin@test.com',87654321,NULL,NULL,NULL,NULL,'2024-06-24','Admin',NULL);
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-25 16:08:48
