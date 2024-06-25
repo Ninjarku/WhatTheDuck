9d7f2eaf628927af71458680bbd319bea5830a3c
Krackern <112253482+Krackern@users.noreply.github.com>
2024-06-25 23:06:20 +0800

    Update indexTest.php

--- tests/unit/indexTest.php
+++ tests/unit/indexTest.php
@@ -1,1 +1,12 @@
-hi
+<?php
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+    public function testIndex()
+    {
+        $this->expectOutputString('Hello, World!');
+        include 'src/index.php';
+    }
+}
+?>
