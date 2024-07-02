072a66022eb1cef41c1fbf07fb36a1082f65213d
Krackern <112253482+Krackern@users.noreply.github.com>
2024-07-02 23:10:10 +0800

    Update LoginTest.php

--- tests/unit/login/LoginTest.php
+++ tests/unit/login/LoginTest.php
@@ -4,7 +4,7 @@
 use PHPUnit\Framework\TestCase;
 
 // Include the login script
-require '/home/student9/docker-volumes/php-docker/whattheduck/process_custlogin.php';
+require '/var/html/www/process_custlogin.php';
 
 class LoginTest extends TestCase
 {
