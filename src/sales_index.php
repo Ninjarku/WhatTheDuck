4f5b69dcab503a2315fea8fdd194be906922d71b
2202282 <114218417+2202282@users.noreply.github.com>
2024-07-04 22:05:06 +0800

    ....

--- src/sales_index.php
+++ src/sales_index.php
@@ -19,7 +19,7 @@ if ($_SESSION["cust_rol"] !== "Sales Admin") {
 
 <head>
     <meta charset="UTF-8">
-    <title>WhatTheDuck - Sales Admin</title>
+    <title>What The Duck - Sales Admin</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- START OF THE LINK -->
