fd81b6a4703542f21d0184b84671edf15aee670e
2200877 <2200877@sit.singaporetech.edu.sg>
2024-07-05 00:57:23 +0800

    Forget Password Stuffs

--- src/ForgetVerify.php
+++ src/ForgetVerify.php
@@ -7,22 +7,22 @@ include "includes/navbar.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WhatTheDuck - Forget Password</title>
-    <link rel="stylesheet"
-          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+    <title>WhatTheDuck - Verify OTP</title>
+    <link rel="stylesheet" 
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
           crossorigin="anonymous">
     <style>
         body, html {
             height: 100%;
             margin: 0;
             font-family: Arial, Helvetica, sans-serif;
-            background-color: #fff5cc;
+            background-color: #fff5cc; /* Light yellow background */
         }
         .container {
             margin-top: 20px;
         }
-        .forget-password-card {
+        .otp-card {
             border: 1px solid #ddd;
             border-radius: 10px;
             text-align: center;
@@ -43,26 +43,27 @@ include "includes/navbar.php";
     </style>
 </head>
 <body>
+    <?php include "includes/navbar.php"; ?>
+
     <div class="container">
-        <h1 class="text-center">Forget Password</h1>
-        <div class="forget-password-card">
-            <h2>Reset Your Password</h2>
-            <p>Please enter your email address to receive a password reset link.</p>
-            <form action="process_sendforgetotp.php" method="post">
+        <div class="otp-card">
+            <h1 class="text-center">Verify OTP</h1>
+            <p>Please enter the OTP sent to your email address.</p>
+            <form action="process_verifyforgetotp.php" method="post">
                 <div class="form-group">
-                    <label for="email">Email address:</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
+                    <label for="otp">OTP:</label>
+                    <input type="text" class="form-control" id="otp" name="otp" required autofocus>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Verify</button>
             </form>
         </div>
     </div>
 
     <?php include 'includes/footer.php'; ?>
 
-    <script defer
-            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
-            integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
+    <script defer 
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js" 
+            integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" 
             crossorigin="anonymous"></script>
 </body>
 </html>
