2d7c55bdab8bd53438d7eeb5a0b6650968646903
Krackern <112253482+Krackern@users.noreply.github.com>
2024-07-06 01:18:19 +0800

    Update AppTest.java

--- tests/uitest/AppTest.java
+++ tests/uitest/AppTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
