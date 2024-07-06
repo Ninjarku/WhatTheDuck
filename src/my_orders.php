51381c96ea219f6d48dbded5093d2511b7c14f51
2202282 <114218417+2202282@users.noreply.github.com>
2024-07-06 17:02:41 +0800

    layout..

--- src/my_orders.php
+++ src/my_orders.php
@@ -54,8 +54,6 @@ if ($_SESSION["cust_rol"] !== "Customer") {
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            display: flex;
-            justify-content: space-between;
         }
 
         .container {
