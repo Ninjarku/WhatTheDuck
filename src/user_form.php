ba0e1fd7142f6c3069ac594975a69b3489c244e7
2202282 <114218417+2202282@users.noreply.github.com>
2024-07-07 19:30:37 +0800

    ...

--- src/user_form.php
+++ src/user_form.php
@@ -23,7 +23,7 @@ $user = [
 
 if ($Form_Type == 1 && $action === 'editUser') {
     $User_ID = isset($_GET['User_ID']) ? intval($_GET['User_ID']) : 0;
-    getUserbyUserID($User_ID);
+    $userData = getUserbyUserID($User_ID);
     if ($userData && is_array($userData)) {
         $user = array_merge($user, $userData);
     }
